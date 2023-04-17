79deb1191f713ab0ece5b0a6ea6f7afb79f0245f
Андрей <TY_1_TA@mail.ru>
2023-04-17 13:42:56 +0700

    Добавьте файлы проекта.

---
+++ ServerMessenger/ServerMessenger.cpp
@@ -0,0 +1,9 @@
+﻿#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+int main () {
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/server_log.txt", 1024 * 1024 * 10, 10);
+    spdlog::logger logger("main", { console_sink, file_sink });
+}
