e176c020fbc22a5d9e12c0d0c6a2ca53ba2a7180
Андрей <TY_1_TA@mail.ru>
2023-04-30 18:04:28 +0700

    Fix

--- ServerMessenger/ServerMessenger.cpp
+++ ServerMessenger/ServerMessenger.cpp
@@ -1,13 +1,12 @@
 ﻿#include "ServerMessenger.h"
 
 int main() {
-    auto logger = std::make_shared<spdlog::logger>(
+    spdlog::register_logger(std::make_shared<spdlog::logger>(
         "main",
         spdlog::sinks_init_list
-            {
-                std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
-                std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/main_log.txt", 1024 * 1024 * 10, 2)
-            });
-    spdlog::register_logger(logger);
+    {
+        std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
+            std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/main_log.txt", 1024 * 1024 * 10, 2)
+    }));
     spdlog::get("main")->info("hello");
 }
